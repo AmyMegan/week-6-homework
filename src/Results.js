4cd97baa7ee9dfbcd6d9df64c3d4877a01b065e7
AmyMegan <amycostall@outlook.com>
2021-06-20 13:06:50 +0100

    Updated Results

--- src/Results.js
+++ src/Results.js
@@ -6,7 +6,7 @@ export default function Results(props){
         return <div className="Results">
             <h2>{props.results.word}</h2>
             {props.results.meanings.map(function(meaning, index) {
-                return meaning.definitions[0].definitions;
+                return meaning.definitions[0].definition;
             })};
         </div>
     } else {
