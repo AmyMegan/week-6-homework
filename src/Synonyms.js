d35841ead5b26a466a0bfb4f84c3a51d09ca333b
AmyMegan <amycostall@outlook.com>
2021-06-21 19:51:59 +0100

    Created Synonyms

---
+++ src/Synonyms.js
@@ -0,0 +1,20 @@
+
+
+export default function Synonyms(props) {
+    console.log(props.synonyms)
+    if (props.synonyms) {
+        return ( <ul className="Synonyms">
+            {props.synonyms.map(function (synonyms, index) {
+            return (
+                <li key={index}>
+                    {synonyms}
+                </li>
+            )
+            })}
+        </ul>
+           
+        )
+    } else {
+        return null;
+    }
+}
