d83c73b87e0ff20c9c6b80931c934e1447d7d5a2
AmyMegan <amycostall@outlook.com>
2021-06-23 17:45:33 +0100

    Created Phonetics to display and produce audio

--- src/Results.js
+++ src/Results.js
@@ -1,23 +1,19 @@
-import Meaning from "./Meaning";
+
+import Phonetic from "./Phonetic";
 
 export default function Results(props){
     console.log(props.results);
     if (props.results) {
         return <div className="Results">
             <h2>{props.results.word}</h2>
-            {props.result.phonetics.map(function (phonetic, index ){
+            {props.results.phonetics.map(function (phonetic, index ){
                 return (
                     <div key={index}>
                         <Phonetic phonetic={phonetic} />
                         </div>
                 );
             }
-            {props.results.meanings.map(function (meaning, index ){
-                return ( <div key={index}>
-                   <Meaning meaning={meaning} />
-                 </div>
-                );
-            })}
+            )}
             </div>
     } else {
         return null;
