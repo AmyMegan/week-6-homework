e0793cef096179e8fcab5f5023d7f9c052573b9e
AmyMegan <amycostall@outlook.com>
2021-06-25 04:37:44 +0100

    Updated React

--- src/Meaning.js
+++ src/Meaning.js
@@ -1,6 +1,6 @@
 import React from "react";
 import Synonyms from "./Synonyms"
-import Meaning from "./Meaning"
+
 
 
 export default function Meaning(props) {
--- src/Results.js
+++ src/Results.js
@@ -1,15 +1,18 @@
 import Phonetic from "./Phonetic";
-
+import Synonyms from "./Synonyms";
+import Meaning from "./Meaning";
 
 export default function Results(props){
     console.log(props.results);
     if (props.results) {
         return <div className="Results">
             <h2>{props.results.word}</h2>
+            <Meaning />
             {props.results.phonetics.map(function (phonetic, index ){
                 return (
                     <div key={index}>
                         <Phonetic phonetic={phonetic} />
+                        <Synonyms />
                         </div>
                 );
             }
