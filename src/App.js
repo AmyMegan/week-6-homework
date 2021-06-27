4c2752a0639defb0e5ee88e3c69299c660424082
AmyMegan <amycostall@outlook.com>
2021-06-27 16:10:39 +0100

    Customizing and Creating Default Keyword to who in display

--- src/App.js
+++ src/App.js
@@ -11,7 +11,7 @@ export default function App() {
     </div>
     </div>
     <main>
-      <Dictonary />
+      <Dictonary defaultKeyword="Paris" />
     </main>
     <small> 
     <footer className="App-footer"> Coded by Amy Megan.</footer>
--- src/Dictonary.js
+++ src/Dictonary.js
@@ -4,38 +4,54 @@ import Results from "./Results";
 import "./Dictonary.css";
 
 
-export default function Dictonary() {
-    let [keyword, setKeyword] = useState(" ")
+export default function Dictonary(props) {
+    let [keyword, setKeyword] = useState(props.defaultKeyword)
     let [results, setResults] = useState(null);
+    let [loaded, setLoaded] = useState(false);
+
 
     function handleResponse(response) {
         console.log(response.data[0]);
         setResults(response.data[0]);
     }
 
-
-
-    function search(event) {
-        event.preventDefault();
-
+    function search() {
         let apiUrl = `https://api.dictionaryapi.dev/api/v2/entries/en_US/${keyword}`;
         axios.get(apiUrl).then(handleResponse)
     }
-     
-
+    function handleSubmit (event) {
+        event.preventDefault();
+        search();
+    }
 
     function handleKeywordChange(event) {
         setKeyword(event.target.value);
         
     }
-    return (
-        <div className="Dictonary"> 
-           <form onSubmit={search}>
-               <input type="search" onChange={handleKeywordChange} autoFocus={true} />
-           </form>
-           <h1>
-           <Results results={results} />
-           </h1>
-        </div>
-    )
+
+    function load() {
+        setLoaded(true);
+        search();
+    }
+
+    if (loaded) {
+        return (
+            <div className="Dictonary">
+                <section>
+               <form onSubmit={handleSubmit}>
+                   <input type="search" onChange={handleKeywordChange} autoFocus={true} />
+               </form>
+               <div className="hint">
+                   suggested words: sunset, wine, yoga, forest, plants...
+               </div>
+               </section>
+               <h1>
+               <Results results={results} />
+               </h1>
+            </div>
+        );
+    } else {
+        load();
+        return "Loading"
+    }
 }
